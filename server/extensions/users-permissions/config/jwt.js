a7acc24143ea1d56e5be60ea2c0d26022980cb4a
keskiju <jukka.keskiluopa@taitounited.fi>
2022-10-07 00:26:18 +0300

    feat: add strapi v4 quickstart app

--- server/extensions/users-permissions/config/jwt.js
+++
@@ -1,8 +0,0 @@
-var fs = require("fs");
-
-module.exports = {
-  jwtSecret:
-    (process.env.JWT_SECRET_FILE &&
-      fs.readFileSync(process.env.JWT_SECRET_FILE, "utf8")) ||
-    "KrfJHuxhL9gmdpHebWHTLfzji34RULpbuiPpCKmd", // Used during build only
-};
