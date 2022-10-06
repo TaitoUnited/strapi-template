917bac06ed979ac75808ae5526bc4cf3f959d1ad
keskiju <jukka.keskiluopa@taitounited.fi>
2022-10-07 02:04:15 +0300

    feat: replace sqlite with pg

--- server/strapi/config/database.js
+++ server/strapi/config/database.js
@@ -1,11 +1,23 @@
-const path = require('path');
+const { readSecret } = require('./utils.js');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USER', 'strapi'),
+      password: readSecret(env('DATABASE_PASSWORD_FILE'), null),
+      schema: env('DATABASE_SCHEMA', 'public'),
+      ssl: env.bool('DATABASE_SSL_ENABLED', false),
     },
-    useNullAsDefault: true,
+    options: {
+      pool: {
+        min: env.int('DATABASE_POOL_MIN'),
+        max: env.int('DATABASE_POOL_MAX'),
+      },
+    },
+    debug: false,
   },
 });
