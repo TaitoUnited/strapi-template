f80e4b1c459fa22255b1d71f72c98c8cab40a083
keskiju <jukka.keskiluopa@taitounited.fi>
2022-10-10 14:33:38 +0300

    chore: skip secrets when RUNNING_BUILD true

--- server/strapi/config/utils.js
+++ server/strapi/config/utils.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const readSecret = (filepath, defaultValue) => {
   const value = filepath && fs.readFileSync(filepath, "utf8");
-  if (!value && process.env.NODE_ENV !== 'development') {
+  if (!value && process.env.RUNNING_BUILD !== 'true') {
     throw new Error(`Secret not found ${filepath}`);
   }
   return value ?? defaultValue;
