4f5aaeba24f10a6c2fc69c272bfb12fb2a9aa9e5
keskiju <jukka.keskiluopa@taitounited.fi>
2022-10-10 15:31:49 +0300

    fix: replace local port on Dockerfile

--- scripts/taito-template/init.sh
+++ scripts/taito-template/init.sh
@@ -63,6 +63,7 @@ sed -i "s/6000/${db_port}/g" \
   scripts/taito/TAITOLESS.md www/README.md &> /dev/null || :
 sed -i "s/9999/${ingress_port}/g" \
   docker-compose.yaml \
+  server/Dockerfile \
   scripts/terraform-dev.yaml \
   scripts/taito/project.sh scripts/taito/env-local.sh \
   scripts/taito/TAITOLESS.md www/README.md &> /dev/null || :
